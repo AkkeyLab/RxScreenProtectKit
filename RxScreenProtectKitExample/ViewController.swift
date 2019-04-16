6985bf7be8f56e284c938b6356136919dd0caf55
AKIO <itaya_akio@matchingagent.co.jp>
2019-04-16 22:04:24 +0900

    :new: Created new example project files

---
+++ RxScreenProtectKitExample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RxScreenProtectKitExample
+//
+//  Created by AKIO on 2019/04/16.
+//  Copyright © 2019 AKIO. All rights reserved.
+//
+
+import RxScreenProtectKit
+import RxSwift
+import UIKit
+
+final class ViewController: UIViewController {
+    // https://www.pakutaso.com
+    @IBOutlet private weak var mainImageView: UIImageView!
+    private let bag = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        isScreenRecord
+            .bind(to: mainImageView.layer.isMosaic)
+            .disposed(by: bag)
+    }
+}
+
+extension ViewController: Mosaicable {}
