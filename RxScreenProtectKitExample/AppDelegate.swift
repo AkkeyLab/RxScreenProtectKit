dd2b7289d4fc0c2870b95b400f45eb2eb82a5678
AKIO <itaya_akio@matchingagent.co.jp>
2019-07-22 14:40:24 +0900

    :sparkles: Remove unused method for example

--- RxScreenProtectKitExample/AppDelegate.swift
+++ RxScreenProtectKitExample/AppDelegate.swift
@@ -17,14 +17,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {}
-
-    func applicationDidEnterBackground(_ application: UIApplication) {}
-
-    func applicationWillEnterForeground(_ application: UIApplication) {}
-
-    func applicationDidBecomeActive(_ application: UIApplication) {}
-
-    func applicationWillTerminate(_ application: UIApplication) {}
 }
