51ef9238b09dbfac8ea073e0cb4933720851ceb4
AKIO <itaya_akio@matchingagent.co.jp>
2019-08-29 15:15:21 +0900

    :new: Add comment

--- Source/ScreenProtectKit.swift
+++ Source/ScreenProtectKit.swift
@@ -20,6 +20,11 @@ public final class ScreenProtectKit {
     internal lazy var isValidState = _isValid.distinctUntilChanged()
 
     private var _isValid = BehaviorRelay<Bool>(value: true)
+    /**
+     You can switch whether or not to subscribe to the screen recording status. Initially enabled.
+     In addition, immediately after switching between disabled and enabled,
+     false will flow once from `isScreenRecord` regardless of whether screen recording is performed.
+    */
     public var isValid: Bool {
         set {
             _isValid.accept(newValue)
