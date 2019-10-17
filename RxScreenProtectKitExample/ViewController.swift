ebb43a287dacd005e0be8c3082b7d90fc8ab8b11
AKIO <itaya_akio@matchingagent.co.jp>
2019-10-17 19:03:40 +0900

    :new: Update example project

--- RxScreenProtectKitExample/ViewController.swift
+++ RxScreenProtectKitExample/ViewController.swift
@@ -10,7 +10,7 @@ import RxScreenProtectKit
 import RxSwift
 import UIKit
 
-final class ViewController: UIViewController {
+final class FirstViewController: UIViewController {
     // https://www.pakutaso.com
     @IBOutlet private weak var mainImageView: UIImageView!
     @IBOutlet private weak var nameLabel: UILabel!
@@ -53,7 +53,6 @@ final class ViewController: UIViewController {
     func changeSettings() {
         let filterCase: [CALayerContentsFilter] = [.linear, .nearest, .trilinear]
         minificationFilterControl.rx.value
-            .skip(1)
             .subscribe(onNext: { [weak self] index in
                 guard let self = self else { return }
                 ScreenProtectKit
@@ -64,7 +63,6 @@ final class ViewController: UIViewController {
             })
             .disposed(by: bag)
         magnificationFilterControl.rx.value
-            .skip(1)
             .subscribe(onNext: { [weak self] index in
                 guard let self = self else { return }
                 ScreenProtectKit
@@ -75,7 +73,6 @@ final class ViewController: UIViewController {
             })
             .disposed(by: bag)
         scaleChanger.rx.value
-            .skip(1)
             .subscribe(onNext: { [weak self] value in
                 guard let self = self else { return }
                 ScreenProtectKit
@@ -84,6 +81,81 @@ final class ViewController: UIViewController {
                             minificationFilter: filterCase[self.minificationFilterControl.selectedSegmentIndex],
                             magnificationFilter: filterCase[self.magnificationFilterControl.selectedSegmentIndex])
                 self.scaleLabel.text = "\(value)"
+            })
+            .disposed(by: bag)
+    }
+}
+
+final class SecondViewController: UIViewController {
+    @IBOutlet private weak var bigImageView: UIImageView!
+    @IBOutlet private weak var normalImageView: UIImageView!
+    @IBOutlet private weak var shortImageView: UIImageView!
+    @IBOutlet private weak var minificationFilterControl: UISegmentedControl!
+    @IBOutlet private weak var magnificationFilterControl: UISegmentedControl!
+    @IBOutlet private weak var pixelBoxSizeChanger: UISlider!
+    @IBOutlet private weak var applyButton: UIButton!
+    @IBOutlet private weak var pixelBoxSizeLabel: UILabel!
+    @IBOutlet private weak var validSwitch: UISwitch!
+    private let bag = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.rx.isScreenRecord
+            .bind(to: bigImageView.layer.rx.isMosaic)
+            .disposed(by: bag)
+        self.rx.isScreenRecord
+            .bind(to: normalImageView.layer.rx.isMosaic)
+            .disposed(by: bag)
+        self.rx.isScreenRecord
+            .bind(to: shortImageView.layer.rx.isMosaic)
+            .disposed(by: bag)
+        applyButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                self?.bigImageView.layer.applyMosaic()
+                self?.normalImageView.layer.applyMosaic()
+                self?.shortImageView.layer.applyMosaic()
+            })
+            .disposed(by: bag)
+        validSwitch.rx.value
+            .subscribe(onNext: { isValid in
+                ScreenProtectKit.shared.isValid = isValid
+            })
+            .disposed(by: bag)
+
+        changeSettings()
+    }
+
+    func changeSettings() {
+        let filterCase: [CALayerContentsFilter] = [.linear, .nearest, .trilinear]
+        minificationFilterControl.rx.value
+            .subscribe(onNext: { [weak self] index in
+                guard let self = self else { return }
+                ScreenProtectKit
+                    .shared
+                    .config(pixelBoxSize: CGFloat(self.pixelBoxSizeChanger.value),
+                            minificationFilter: filterCase[index],
+                            magnificationFilter: filterCase[self.magnificationFilterControl.selectedSegmentIndex])
+            })
+            .disposed(by: bag)
+        magnificationFilterControl.rx.value
+            .subscribe(onNext: { [weak self] index in
+                guard let self = self else { return }
+                ScreenProtectKit
+                    .shared
+                    .config(pixelBoxSize: CGFloat(self.pixelBoxSizeChanger.value),
+                            minificationFilter: filterCase[self.minificationFilterControl.selectedSegmentIndex],
+                            magnificationFilter: filterCase[index])
+            })
+            .disposed(by: bag)
+        pixelBoxSizeChanger.rx.value
+            .subscribe(onNext: { [weak self] value in
+                guard let self = self else { return }
+                ScreenProtectKit
+                    .shared
+                    .config(pixelBoxSize: CGFloat(self.pixelBoxSizeChanger.value),
+                            minificationFilter: filterCase[self.minificationFilterControl.selectedSegmentIndex],
+                            magnificationFilter: filterCase[self.magnificationFilterControl.selectedSegmentIndex])
+                self.pixelBoxSizeLabel.text = "\(value)"
             })
             .disposed(by: bag)
     }
